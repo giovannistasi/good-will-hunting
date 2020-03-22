dde09a0cc9fcf591084ef6246d3d3b47cdec6152
giovannistasi <gsstasi@gmail.com>
2020-03-22 13:43:21 +0100

    feat: polished job form; fix: changed styling of about page to inline

--- client/src/components/JobForm.js
+++ client/src/components/JobForm.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, DatePicker, Select } from 'antd';
 
 function JobForm () {
 
+  const [date, setDate] = useState('');
+  const [maxParticipants, setMaxParticipants] = useState(1);
+
   const { Option } = Select;
   const { TextArea } = Input;
-
-
-  function onChange (value, dateString) {
-    console.log('Selected Time: ', value);
-    console.log('Formatted Selected Time: ', dateString);
-  }
-
-  function onOk (value) {
-    console.log('onOk: ', value);
-  }
-
-  function changeMaxParticipants(value) {
-    console.log(`Selected: ${value}`);
-  }
 
   const options = [];
   for (let i = 1; i < 10; i++) {
@@ -26,23 +15,60 @@ function JobForm () {
   }
   options.push(<Option key="10+">10+</Option>);
 
-  return (
-    <div style={{ display: 'flex' }}>
-      <div>
-        <Form>
-          <Form.Item >
-            <Input></Input>
-          </Form.Item>
-          <Form.Item>
-            <Select defaultValue="1" onChange={changeMaxParticipants} style={{ width: 200 }}>
-              {options}
-            </Select>
-          </Form.Item>
-        </Form>
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 14 },
+  };
 
-        <TextArea rows={4} />
-      </div>
-      <DatePicker showTime={{ format: 'HH' }} onChange={onChange} onOk={onOk} />
+  function onChange (value, dateString) {
+    console.log('Selected Time: ', value);
+    console.log('Formatted Selected Time: ', dateString);
+  }
+
+  function timeSelect (value) {
+    console.log('onOk: ', value);
+  }
+
+  function changeMaxParticipants (value) {
+    console.log(`Selected: ${value}`);
+  }
+
+  function submitForm (values) {
+
+  }
+
+  return (
+    <div style={{
+      'justify-content': 'center',
+      'display': 'flex',
+      'flex-direction': 'column'
+    }}>
+      <h1 style={{'margin': 'auto', 'padding-bottom': '3vh'}}>Insert a new job</h1>
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 14 }}
+        {...layout}
+        style={{ width: '60vw', height: "50vh" }}
+        onFinish={submitForm}
+      >
+        <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+          <Input></Input>
+        </Form.Item>
+        <Form.Item name="description" label="Description" rules={[{ required: true }]}>
+          <TextArea autoSize={{ minRows: 4, maxRows: 7 }} />
+        </Form.Item>
+        <Form.Item name="address" label="Address" rules={[{ required: true }]}>
+          <Input></Input>
+        </Form.Item>
+        <Form.Item name="maxParticipants" label="Participants" rules={[{ required: true }]}>
+          <Select placeholder="How many people can apply?" onChange={changeMaxParticipants} >
+            {options}
+          </Select>
+        </Form.Item>
+        <Form.Item name="eventTime" label="Date and time" rules={[{ required: true }]}>
+          <DatePicker showTime={{ format: 'HH' }} onOk={timeSelect} />
+        </Form.Item>
+      </Form>
     </div>
   )
 }
