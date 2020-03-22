e5f8ece6e07b89eeaa386f92618330061dbf1e2d
ik845 <ila.krishnamoorthy845@gmail.com>
2020-03-21 21:36:20 -0400

    about page done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Carousel } from 'antd';
+import './About.css';
 
-function About () {
+function About() {
   return (
-    <div>This is the about page</div> 
-  )
+    <div>
+      <Carousel>
+        <div>
+          <h1>
+            In recent years rates of volunteering and community service have
+            been steadily trending downward.<br></br> For a lot of us, our
+            online community feels a lot closer to us than our physical one
+            does. Our current market doesn't value these things anymore. That's
+            why we are striving to merge these two worlds...
+          </h1>
+        </div>
+        <div>
+          <h1>
+            Here at Good Will Hunting we hope to bring the focus back to our
+            local communities by creating an online platform and unique
+            incentives program to increase volunteerism and civic engagement.
+          </h1>
+        </div>
+        <div>
+          <h1>
+            Community members in need can request the help of volunteers within
+            their respective communities with the appropriate skills. For
+            instance,<br></br> a neighbor who has a plumbing issue but can't
+            afford to call a professinal,<br></br> a parent who is looking for a
+            math tutor but is living paycheck to paycheck,<br></br> or an
+            elderly person who is looking for a ride to the doctors.<br></br> In
+            return, volunteers would receive credits, or Good Will Points, for
+            each hour they spend fulfilling these requests.
+          </h1>
+        </div>
+        <div>
+          <h1>
+            We hope to soon bring in local businesses where volunteers can
+            redeem thier Good Will Points for things like hot meals and
+            household essentials.<br></br>
+            <br></br> Small Steps For Big Change!
+          </h1>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default About;
