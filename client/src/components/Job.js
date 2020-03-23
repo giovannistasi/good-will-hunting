574a040a8dec9a3c0beadc7c18266595af19873f
giovannistasi <gsstasi@gmail.com>
2020-03-23 11:43:36 +0100

    feat: add card to job listing page

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -2,17 +2,38 @@ import React, { useContext } from 'react';
 import { Context } from '../global/Store';
 import { useParams } from 'react-router-dom';
 
+import { Card, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+
 function Job () {
 
   const { id } = useParams();
   const [state, dispatch] = useContext(Context);
   const job = state.jobs.find(job => job.listingId === id);
-  
+
   return (
-    <div>
-      <div>This is listing {id} </div> 
-      <div>{job.description}</div>
-    </div>
+    <Card
+      style={{ width: 500 }}
+      cover={
+        <img
+          alt="example"
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+        />
+      }
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+    >
+      <Meta
+        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+        title={job.title}
+        description={job.description}
+      />
+    </Card>
   )
 }
 
