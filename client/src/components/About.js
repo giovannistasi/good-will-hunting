b36f8c314c1dd09e52eefffe8ed605cd34dd02d1
Richard Perkins <richardjperkins89@gmail.com>
2020-03-25 23:24:44 +0100

    feat: updated About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,24 +9,26 @@ function About () {
           'textAlign': 'center',
           'height': '520px',
           'lineHeight': '45px',
-          'background': '#4266a3',
+          'background': '#0D2056',
           'overflow': 'hidden'
         }}
       >
         <div>
           <h1
             style={{
-              'margin': '8% 8% 0 8%',
+              'marginTop': '8%',
+              'padding': '0 15%',
               'fontStyle': 'oblique',
               'fontSize': '25px',
               'color': '#fff'
             }}
           >
             In recent years rates of volunteering and community service have
-            been steadily trending downward.<br></br> For a lot of us, our
-          online community feels a lot closer to us than our physical one
-          does. Our current market doesn't value these things anymore. That's
-          why we are striving to merge these two worlds...
+            been steadily trending downward.
+            <br /><br />
+            For many of us, our online community feels a lot closer to us than our physical one.
+            Our current market doesn't value these things anymore. That's
+            why we are striving to merge these two worlds...
           </h1>
         </div>
         <div>
@@ -41,6 +43,9 @@ function About () {
             Here at Good Will Hunting we hope to bring the focus back to our
             local communities by creating an online platform and unique
             incentives program to increase volunteerism and civic engagement.
+            <br></br>
+            Community members in need can request the help of volunteers within
+            their respective communities with the appropriate skills.
           </h1>
         </div>
         <div>
@@ -52,14 +57,15 @@ function About () {
               'color': '#fff'
             }}
           >
-            Community members in need can request the help of volunteers within
-            their respective communities with the appropriate skills. For
-            instance, a neighbor who has a plumbing issue but can't
-            afford to call a professinal,<br></br> a parent who is looking for a
-            math tutor but is living paycheck to paycheck,<br></br> or an
-            elderly person who is looking for a ride to the doctors.<br></br> In
-          return, volunteers would receive credits, or Good Will Points, for
-          each hour they spend fulfilling these requests.
+            For instance, a neighbor who has a plumbing issue but can't
+            afford to call a profesional,
+            <br></br>
+            a parent who is looking for a math tutor but is living paycheck to paycheck,
+            <br></br>
+            or an elderly person who is looking for a ride to the doctors.
+            <br></br>
+            In return, volunteers would receive credits, or Good Will Points, for
+            each hour they spend fulfilling these requests.
           </h1>
         </div>
         <div>
@@ -72,9 +78,11 @@ function About () {
             }}
           >
             We hope to soon bring in local businesses where volunteers can
-            redeem thier Good Will Points for things like hot meals and
-            household essentials.<br></br>
-            <br></br> Small Steps For Big Change!
+            redeem their Good Will Points for things like hot meals and
+            household essentials.
+            <br></br>
+            <br></br>
+            Small Steps For Big Change!
           </h1>
         </div>
       </Carousel>
