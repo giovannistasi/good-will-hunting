e32a181c8b2e2ab63fc188707c5c181c8221e045
Richard Perkins <richardjperkins89@gmail.com>
2020-03-25 17:44:33 +0100

    updated listing model and migration file to include latitude ad longitude values

--- server/models/listing.js
+++ server/models/listing.js
@@ -26,6 +26,12 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       type: DataTypes.TEXT
     },
+    latitude: {
+      type: DataTypes.DECIMAL
+    },
+    longitude: {
+      type: DataTypes.DECIMAL
+    },
     creditValue: {
       allowNull: false,
       type: DataTypes.DECIMAL
