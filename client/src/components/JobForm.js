c8be1370e9a670a98cc9a0aefa73be9a6267e940
giovannistasi <gsstasi@gmail.com>
2020-03-25 20:27:39 +0100

    fix: styling on job form input

--- client/src/components/JobForm.js
+++ client/src/components/JobForm.js
@@ -7,7 +7,7 @@ import PlacesAutocomplete, {
   getLatLng,
 } from 'react-places-autocomplete';
 
-function JobForm() {
+function JobForm () {
   const [eventTime, setEventTime] = useState(new Date());
   const [maxParticipants, setMaxParticipants] = useState(1);
   const [creditValue, setCreditValue] = useState(1);
@@ -32,19 +32,19 @@ function JobForm() {
     wrapperCol: { span: 14 },
   };
 
-  function timeSelect(value) {
+  function timeSelect (value) {
     setEventTime(value._d);
   }
 
-  function changeMaxParticipants(value) {
+  function changeMaxParticipants (value) {
     setMaxParticipants(value);
   }
 
-  function changeCreditValue(value) {
+  function changeCreditValue (value) {
     setCreditValue(value);
   }
 
-  function submitForm(values) {
+  function submitForm (values) {
     const { title, description, address } = values;
     const formData = {
       title,
@@ -71,119 +71,119 @@ function JobForm() {
       {jobId ? (
         <Redirect to={'/job/' + jobId} />
       ) : (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-          }}
-        >
-          <h1 style={{ margin: 'auto', paddingBottom: '3vh' }}>
-            Create a new job
-          </h1>
-          <Form
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 14 }}
-            {...layout}
-            style={{ width: '60vw', height: '50vh' }}
-            onFinish={submitForm}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+            }}
           >
-            <Form.Item name="title" label="Title" rules={[{ required: true }]}>
-              <Input></Input>
-            </Form.Item>
-            <Form.Item
-              name="description"
-              label="Description"
-              rules={[{ required: true }]}
+            <h1 style={{ margin: 'auto', paddingBottom: '3vh' }}>
+              Create a new job
+          </h1>
+            <Form
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 14 }}
+              {...layout}
+              style={{ width: '60vw', height: '50vh' }}
+              onFinish={submitForm}
             >
-              <TextArea autoSize={{ minRows: 4, maxRows: 7 }} />
-            </Form.Item>
-            <Form.Item
-              name="address"
-              label="Address"
-              rules={[{ required: true }]}
-            >
-              <div>
-                <PlacesAutocomplete
-                  value={address}
-                  onChange={setAddress}
-                  onSelect={handleSelect}
-                >
-                  {({
-                    getInputProps,
-                    suggestions,
-                    getSuggestionItemProps,
-                    loading,
-                  }) => (
-                    <div>
-                      <p>Latitude: {coordinates.lat}</p>
-                      <p>Longitude: {coordinates.lng}</p>
-
-                      <input
-                        {...getInputProps({ placeholder: 'Type address' })}
-                      />
-
-                      <div>
-                        {loading ? <div>...loading</div> : null}
-
-                        {suggestions.map(suggestion => {
-                          const style = {
-                            backgroundColor: suggestion.active
-                              ? '#41b6e6'
-                              : '#fff',
-                          };
-
-                          return (
-                            <div
-                              {...getSuggestionItemProps(suggestion, { style })}
-                            >
-                              {suggestion.description}
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </div>
-                  )}
-                </PlacesAutocomplete>
-              </div>
-            </Form.Item>
-            <Form.Item
-              name="maxParticipants"
-              label="Participants"
-              rules={[{ required: true }]}
-            >
-              <Select
-                placeholder="How many people can apply?"
-                onChange={changeMaxParticipants}
+              <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+                <Input></Input>
+              </Form.Item>
+              <Form.Item
+                name="description"
+                label="Description"
+                rules={[{ required: true }]}
               >
-                {options}
-              </Select>
-            </Form.Item>
-            <Form.Item
-              name="creditValue"
-              label="Credits rewarded"
-              rules={[{ required: true }]}
-            >
-              <InputNumber
-                defaultValue={1}
-                onChange={changeCreditValue}
-              ></InputNumber>
-            </Form.Item>
-            <Form.Item
-              name="eventTime"
-              label="Date and time"
-              rules={[{ required: true }]}
-            >
-              <DatePicker showTime={{ format: 'HH' }} onOk={timeSelect} />
-            </Form.Item>
-            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
+                <TextArea autoSize={{ minRows: 4, maxRows: 7 }} />
+              </Form.Item>
+              <Form.Item
+                name="address"
+                label="Address"
+                rules={[{ required: true }]}
+              >
+                <div>
+                  <PlacesAutocomplete
+                    value={address}
+                    onChange={setAddress}
+                    onSelect={handleSelect}
+                  >
+                    {({
+                      getInputProps,
+                      suggestions,
+                      getSuggestionItemProps,
+                    }) => (
+                        <div>
+                          <p>Latitude: {coordinates.lat}</p>
+                          <p>Longitude: {coordinates.lng}</p>
+                          <div className="ant-form-item-control-input">
+                            <div className="ant-form-item-control-input-content">
+                              <input className="ant-input"
+                                {...getInputProps({ placeholder: 'Type address' })}
+                              />
+                            </div>
+                          </div>
+                          {suggestions.map(suggestion => {
+                            const style = {
+                              backgroundColor: suggestion.active
+                                ? '#41b6e6'
+                                : '#fff',
+                              padding: '10px',
+                              'z-index': 2047483638,
+                              'box-shadow': '0px 8px 16px 0px rgba(0,0,0,0.2)'
+                            };
+                            return (
+                              <div
+                                {...getSuggestionItemProps(suggestion, { style })}
+                              >
+                                {suggestion.description}
+                              </div>
+
+                            );
+                          })}
+                        </div>
+                      )}
+                  </PlacesAutocomplete>
+                </div>
+              </Form.Item>
+              <Form.Item
+                name="maxParticipants"
+                label="Participants"
+                rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="How many people can apply?"
+                  onChange={changeMaxParticipants}
+                >
+                  {options}
+                </Select>
+              </Form.Item>
+              <Form.Item
+                name="creditValue"
+                label="Credits rewarded"
+                rules={[{ required: true }]}
+              >
+                <InputNumber
+                  defaultValue={1}
+                  onChange={changeCreditValue}
+                ></InputNumber>
+              </Form.Item>
+              <Form.Item
+                name="eventTime"
+                label="Date and time"
+                rules={[{ required: true }]}
+              >
+                <DatePicker showTime={{ format: 'HH' }} onOk={timeSelect} />
+              </Form.Item>
+              <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
               </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      )}
+              </Form.Item>
+            </Form>
+          </div>
+        )}
     </Route>
   );
 }
